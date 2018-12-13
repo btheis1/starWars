5c4873ecbf583a48e9dee58b1f864cbc58d1d321
C Howe <brook.theis@gmail.com>
2018-12-12 21:30:22 -0700

    try again

--- server.js
+++ server.js
@@ -74,16 +74,20 @@ app.get("/api/characters/:character", function(req, res) {
 app.post("/api/characters", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
+
+  //entire object that we built on the front end
   var newcharacter = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  //creates the route name part of the object. 
   newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
   console.log(newcharacter);
 
+  //add newcharacter object to the characters object
   characters.push(newcharacter);
-
+  //send the json object to the webpage. As a resopnse. 
   res.json(newcharacter);
 });
 
